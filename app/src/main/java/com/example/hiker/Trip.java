fbdfe3f376405deb11569f641d5431daa50ed88f
Alex Fleming <amfleming3861@gmail.com>
2019-07-26 00:05:31 -0400

    renamed variables to depature and returndate

--- app/src/main/java/com/example/hiker/Trip.java
+++ app/src/main/java/com/example/hiker/Trip.java
@@ -2,8 +2,8 @@ package com.example.hiker;
 
 public class Trip {
     private long tr_id;
-    private String date;
-    private String description;
+    private String departure;
+    private String returndate;
 
     //this constructor called when adding a trip to a trail. create the trip object with the trail_id of the trail its associated with.
     public Trip (long tr_id){
@@ -11,29 +11,29 @@ public class Trip {
     }
 
     //this constructor is called when building a List<Trip> for populating list of trips in Trail_activity
-    public Trip(long tr_id, String date, String description){
+    public Trip(long tr_id, String departure, String returndate){
         this.tr_id=tr_id;
-        this.date=date;
-        this.description=description;
+        this.departure = departure;
+        this.returndate = returndate;
     }
 
 
-    public void setDate(String date){
-        this.date = date;
+    public void setDeparture(String departure){
+        this.departure = departure;
     }
-    public void setDescription(String description){
-        this.description = description;
+    public void setReturndate(String returndate){
+        this.returndate = returndate;
     }
 
     public long getTr_id() {
         return tr_id;
     }
 
-    public String getDate() {
-        return date;
+    public String getDeparture() {
+        return departure;
     }
 
-    public String getDescription() {
-        return description;
+    public String getReturndate() {
+        return returndate;
     }
 }
